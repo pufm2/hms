d6d89e56e4e31926d979eb0c9e68b5263110678e
lxhoan <lxhoan@gmail.com>
2012-03-16 12:34:29 +0700

    add maven project

---
+++ src/main/java/puf/m2/hms/App.java
@@ -0,0 +1,13 @@
+package puf.m2.hms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
