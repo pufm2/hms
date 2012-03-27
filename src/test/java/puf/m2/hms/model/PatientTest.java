fbe8276cee6298fc58643ca0df4a1c185666ab80
lxhoan <lxhoan@gmail.com>
2012-03-28 00:35:26 +0700

    add a unit test as an example for Thu

---
+++ src/test/java/puf/m2/hms/model/PatientTest.java
@@ -0,0 +1,21 @@
+package puf.m2.hms.model;
+
+import org.junit.Test;
+
+public class PatientTest {
+
+    @Test
+    public void testAddPatient() {
+        
+    }
+    
+    @Test
+    public void testUpdatePatient() {
+        
+    }
+    
+    @Test
+    public void testLookingForPatient() {
+        
+    }
+}
