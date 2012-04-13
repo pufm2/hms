5629b33409de8ca3f890fbb63a597f465045dc9d
lxhoan <lxhoan@gmail.com>
2012-04-12 17:11:34 -0700

    use DatabaseFactory

--- src/main/java/puf/m2/hms/db/SqliteDatabase.java
+++ src/main/java/puf/m2/hms/db/SqliteDatabase.java
@@ -5,8 +5,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SqliteDatabase implements Database {
 
