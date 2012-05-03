acc53c8959f0f4ed7778e51ac7cf3f3c89a28de0
lxhoan <lxhoan@gmail.com>
2012-05-03 23:21:48 +0700

    commit

--- src/main/java/puf/m2/hms/exception/HmsException.java
+++ src/main/java/puf/m2/hms/exception/HmsException.java
@@ -4,7 +4,17 @@ public class HmsException extends Exception {
 
     private static final long serialVersionUID = 1199072650805830161L;
 
+    private String message;
+    
     public HmsException(Throwable cause) {
         super(cause);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
