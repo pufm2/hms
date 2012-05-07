2a6a259840dbf4533aeec4dcb90f514f6857e04c
lxhoan <lxhoan@gmail.com>
2012-05-07 15:33:31 +0700

    change the way open/close db

--- src/main/java/puf/m2/hms/run/RunHMS.java
+++ src/main/java/puf/m2/hms/run/RunHMS.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
+import puf.m2.hms.db.DbException;
+import puf.m2.hms.model.HmsEntity;
 import puf.m2.hms.view.Login;
 import puf.m2.hms.view.Utils;
 
@@ -14,8 +16,10 @@ public class RunHMS {
 	 * Management course at PUF - MINF2010 Supervisor: Prof. Emilie Balland
 	 * Students: NGUYEN Huu Phat, LE Xuan Hoan, TRAN Le Nhu Quynh, NGO Minh Anh
 	 * Thu Mar-May 2012
+	 * @throws DbException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws DbException {
+		HmsEntity.DB.createConnection();
 	    UIManager.put("swing.boldMetal", Boolean.FALSE);
 
 		SwingUtilities.invokeLater(new Runnable() {
